751e5492aa925e249376daffac28b79a07637eac
Sanrrone <sandrolvalenzuelad@gmail.com>
2016-11-14 18:13:48 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# simulationsMethods
-the first module of SEPA to simulate reads from genomes
+![banner](https://raw.githubusercontent.com/microgenomics/tutorials/master/img/microgenomics.png)
+
+#simulationsMethods
+--------------------
+
+##Usage
+* Bash >= v4 (tested in Linux)
+* [Metasim](http://ab.inf.uni-tuebingen.de/software/metasim/) with desired genomes
+
+##Usage
+
+	bash simulationsMethods.bash --cfile [config file]
+
+##Configuration file
+This file contain several parameters to steer the script:
+
+	SEPAHOME=/Users/castrolab04/Desktop/SEPA
+	BEGIN_FOLDER=Simulation
+	GENOMESIZEBALANCE=B
+	SPECIES=10	#this means that the program take 10 fastas randomly to make the simulation 
+	DEEPSEQUENCE=100000
+	DOMINANCE=10
+	READSIZE=75,150,300,1000
+	PERMAMENT=35
+	THREADS=16
+	GENOME_DB=/Users/castrolab04/DB/mydb.fasta
+	METASIMFOLDER=/Users/castrolab04/programs/metasim
+	
+where:
+
+* SEPAHOME is the folder where is SEPA.
+* BEGIN_FOLDER is the initial folder to do the simulations, the folder must exist.
+* GENOMESIZEBALANCE is to specify the type of your DB (B,V,F means bacteria, virus and fungi), you can combine the types tipying BV,VF or BVF.
+* SPECIES is the number of species to take randomly
+* DEEPSEQUENCE is the X deep to simulate like a sequencer
+* DOMINANCE is the % on the number of genomes that takes the majority of reads:
+	* 1 -> one genome take 50% of the reads.
+	* 10 -> 10% of species takes the 25% of the reads.
+	* 50 -> 50% of species takes the 80% of the reads.
+	* 100 -> all species have equal reads abundance.
+* READSIZE is the size of the simulated read (75,150,300 or 1000). Note: no error sustitution are considered.
+* PERMAMENT is the fasta with X number that always be in the selections SPECIES.
+* THREADS is the number of threads for metasim
+* GENOME_DB is the path (and file), of the your genomes DB, this DB will splited into individual fastas (that fastas will be in metasim).
+* METASIMFOLDER is the folder of Metasim.sh is.
---
+++ simulationsMethods.bash
@@ -0,0 +1,210 @@
+if [[ "$@" =~ "--debug" ]]; then
+	set -ex
+else
+	set -e
+fi
+
+for i in "$@"
+do
+	case $i in
+	"--cfile")
+		cfileband=1
+		invalidband=0
+	;;
+	"--rfile")
+		rfileband=1
+		invalidband=0
+	;;
+	"--help")
+	invalidband=0
+		echo "#########################################################################################"
+		echo -e "\nUsage: bash simulationMethods --cfile [config file]"
+		echo -e "\nOptions aviable for config file:"
+		echo "GENOMESIZEBALANCE is the type of genome, for example 'B' for bacteria, 'V' for virus, etc."
+		echo "--rfile reads file, if you have paired end reads, use: --rfile readfile1.fa,readfile2.fa"
+		
+		echo -e "\n#########################################################################################"
+		exit
+	;;
+	*)
+		
+		if [ $((cfileband)) -eq 1 ];then
+
+			if ! [ -f $i ];then
+				echo "$i file no exist"
+				exit
+			fi
+
+			for parameter in `awk '{print}' $i`
+			do
+				Pname=`echo "$parameter" |awk 'BEGIN{FS="="}{print $1}'`		
+				case $Pname in
+					"SEPA_HOME")
+						SEPA_HOME=$(echo "$parameter" | awk 'BEGIN{FS="="}{print $2}' | sed "s/,/ /g")
+					;;
+					"GENOMESIZEBALANCE")
+						GENOMESIZEBALANCE=$(echo "$parameter" | awk 'BEGIN{FS="="}{print $2}' | sed "s/,/ /g")
+					;;
+					"BEGIN_FOLDER")
+						BEGIN_FOLDER=$(echo "$parameter" | awk 'BEGIN{FS="="}{print $2}' | sed "s/,/ /g")				
+					;;
+					"SPECIES")
+						SPECIES=$(echo "$parameter" | awk 'BEGIN{FS="="}{print $2}' | sed "s/,/ /g")				
+					;;
+					"DEEPSEQUENCE")
+						ABUNDANCE=$(echo "$parameter" | awk 'BEGIN{FS="="}{print $2}' | sed "s/,/ /g")					
+					;;
+					"DOMINANCE")
+						DOMINANCE=$(echo "$parameter" | awk 'BEGIN{FS="="}{print $2}' | sed "s/,/ /g")					
+					;;
+					"READSIZE")
+						READSIZE=$(echo "$parameter" | awk 'BEGIN{FS="="}{print $2}' | sed "s/,/ /g")					
+					;;
+					"METHOD")
+						METHOD=$(echo "$parameter" | awk 'BEGIN{FS="="}{print $2}' | sed "s/,/ /g")				
+					;;
+					"THREADS")
+						THREADS=$(echo "$parameter" | awk 'BEGIN{FS="="}{print $2}' | sed "s/,/ /g")			
+					;;
+					"GENOME_DB")
+						GENOME_DB=$(echo "$parameter" | awk 'BEGIN{FS="="}{print $2}' | sed "s/,/ /g")	
+					;;
+					"METASIMFOLDER")
+						METASIMFOLDER=$(echo "$parameter" | awk 'BEGIN{FS="="}{print $2}' | sed "s/,/ /g")
+					;;
+					"PERMANENT")
+						PERMANENT=$(echo "$parameter" | awk 'BEGIN{FS="="}{print $2}' | sed "s/,/ /g")
+					;;
+				esac
+			done
+			
+
+			if [ "$COORDFOLDER" == "" ];then
+				COORDFOLDER=$HOME
+			fi
+			statusband=$((statusband+1))
+			cfileband=0
+
+			if [ "$GENOME_DB" == "" ];then
+				echo "no GENOME_DB was spcified"
+				exit
+			fi
+		fi
+
+	;;
+	esac
+done
+
+
+################################################################
+################################################################
+if [ -d $BEGIN_FOLDER ]; then
+	cd $BEGIN_FOLDER
+else
+	echo "$BEGIN_FOLDER no exist, impossible to continue"
+	exit
+fi
+
+
+for a in $GENOMESIZEBALANCE
+do
+
+	if [ -d $a ]; then
+		cd $a
+		if [ -f fasta_0.fasta ]; then
+			echo "Fastas here, continue"
+		else
+			echo "Spliting $GENOME_DB"
+			awk -f ${SEPA_HOME}/Modules/scripts/splitmultifasta.awk $GENOME_DB  #one fasta per entry wil be generate.
+		fi
+	else
+		mkdir $a
+		cd $a
+		echo "Spliting $GENOME_DB" 
+	
+		awk -f ${SEPA_HOME}/Modules/scripts/splitmultifasta.awk $GENOME_DB  #a lot of fastas wil be generate.
+
+	fi
+	
+	echo "GBalance $a" 
+	echo "DB: $GENOME_DB"
+
+		for c in $SPECIES
+		do
+			echo "------Species: $c" 
+			total=`ls -1 *.fasta |wc -l |awk '{if($1<0){print "no fastas"}else{print $1}}'` #-1= less species folder
+			if [ $((total)) -le 0  ];then
+				echo "no fastas for selection"
+				exit
+			fi
+
+			if [ -d species_$c ]; then
+				cd species_$c
+			else
+				mkdir species_$c
+				cd species_$c
+				echo "------Calling subpipe SpecieSelection" 
+				
+				if [ $((total)) -le $((c))  ]; then
+					bash ${SEPA_HOME}/Modules/scripts/SpecieSelection.bash "${SEPA_HOME}" "$GENOME_DB" "$PERMANENT" "$total"
+				else
+					bash ${SEPA_HOME}/Modules/scripts/SpecieSelection.bash "${SEPA_HOME}" "$GENOME_DB" "$PERMANENT" "$c"
+				fi
+				
+			fi
+
+			for d in $ABUNDANCE
+			do
+				echo "---------Abundance: $d" 
+				if [ -d abundance_$d ]; then
+					cd abundance_$d
+				else
+					mkdir abundance_$d
+					cd abundance_$d
+					mv ../*.fasta .
+				fi
+
+				for e in $DOMINANCE
+				do
+					echo "------------Dominance: $e" 
+					if [ -d dominance_$e ]; then
+						cd dominance_$e
+					else
+						mkdir dominance_$e
+						cd dominance_$e
+						mv ../*.fasta .
+						gipermanent=`grep ">" fasta_$PERMANENT.fasta | awk 'BEGIN{FS="|"}{print $4}'`
+						bash ${SEPA_HOME}/Modules/scripts/DomainDistribution.bash "$SEPA_HOME" "$c" "$d" "$e" "$gipermanent"
+						for rsize in $READSIZE
+						do
+							echo "dataset_A$d""_R$rsize	$d	$rsize	popu$c""Abun$d""Dom$e.mprf" >> metasim_simulation_table
+						done
+
+						echo "------------execute metasim scripts" 
+
+						bash ${SEPA_HOME}/Modules/scripts/Metasim.sh "$THREADS" "$METASIMFOLDER"
+						bash ${SEPA_HOME}/Modules/scripts/SplitMetasimPairs.sh "${SEPA_HOME}"
+						for rsize in $READSIZE
+						do
+							rm dataset_A$d""_R$rsize/*.fa.gz #to save disk space
+						done
+
+
+					fi
+
+				mv *.fasta ../.				
+				cd ..
+				done #done e
+
+			mv *.fasta ../.
+			cd ..
+			done #done d
+
+		mv *.fasta ../.
+		cd ..		
+		done #done c
+
+cd ..
+done #done a
+
+cd ..
